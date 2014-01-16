be8c9e7e6243c261b296ab94969676602854d7c2
nhayes-roth <nathan.hayesroth@gmail.com>
2014-01-15 21:15:14 -0700

    1.3

--- ArraysAndStrings.java
+++ ArraysAndStrings.java
@@ -62,10 +62,37 @@ public class ArraysAndStrings {
 		}
 	}
 
+	/* 1.3
+	 * Design an algorithm and write code to remove the duplicate characters 
+	 * in a string without using any additional buffer.
+	 * NOTE: One or two additional variables are fine. 
+	 * 		 An extra copy of the array is not.
+	 */
+	public static String removeDuplicateCharacters(String str){
+		for (int i = 0; i < str.length(); i++){
+			for (int j = i + 1; j < str.length(); j++){
+				if (str.charAt(i) == str.charAt(j)){
+					str = str.substring(0, j) + str.substring(j+1);
+				}
+			}
+		}
+		return str;
+	}
+
+	public static void testRemoveDuplicates(){
+		String [] to_test = {"", "a", "abbaccd", "abcdeff"};
+		for (String str : to_test) {
+			System.out.println(removeDuplicateCharacters(str));
+		}
+	}
+
+	/* main method */
 	public static void main(String[] args) {
 		// 1.1
 		// testNoRepeats();
 		// 1.2
-		testReverse();
+		// testReverse();
+		// 1.3
+		testRemoveDuplicates();
 	}
 }
